751711e2be6ab1e0bab5de1dc9022a4bc0970243
Syriamanal <Shadinachat123@gmail.com>
2014-02-25 08:21:51 -0800

    Total re write! no bugs now!

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include "stdafx.h"
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
-#include <Windows.h>
+//#include <Windows.h>
 
 using namespace std;
 
 
 int main()
 {
-	char Map = [5] [5] = {{'#' '#' '#' '#' '#'}
-	                      {'#' ''}
+    char Map [10] [10] = {{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+	                  {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                          {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                          {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+	                  {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
 	};
+	
+	for(int row = 0; row < 10; row++)
+	{
+		for (int colum = 0; colum < 10; colum++)
+		{
+			cout << Map [row] [colum] << " ";
+		}
+		cout << endl;
+	}
+	
+	
+	
+    return 0;
 }
