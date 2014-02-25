9b3e6b12d345e094bf03a136ed0f00d5cb637496
Syriamanal <Shadinachat123@gmail.com>
2014-02-25 07:39:55 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,14 @@
-// MyFirstConsoleGame.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-// MyFirstConsoleGame.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include <Windows.h>
 
 using namespace std;
 
-/*
-class Maps {
-public:
-	char map[5][5] =
-	{
-		{ '#', '#', '#', '#', '#' },
-		{ '#',                '#' },
-		{ '#',                '#' },
-		{ '#',                '#' },
-		{ '#', '#', '#', '#', '#' }
-	};//Char end
-};
-*/
-
 
 int main()
 {
-	char map[5][5] =
-	{
-		{ '#', '#', '#', '#', '#' },
-		{ '#',                '#' },
-		{ '#',                '#' },
-		{ '#',                '#' },
-		{ '#', '#', '#', '#', '#' }
-	};//Char end
-	cout << map[5] [5] << endl;
-
+	char Map = [5] [5] = {{'#' '#' '#' '#' '#'}
+	                      {'#' ''}
+	};
 }
