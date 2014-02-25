27361ed9649596147e86e87996012f0e4150f72b
Syriamanal <Shadinachat123@gmail.com>
2014-02-24 19:00:40 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+
+void Map ()
+{
+  char Map[5] [5] = {'#' '#' '#' '#' '#' 
+                     '#'             '#' 
+                     '#'             '#'
+                     '#' '#' '#' '#' '#' };
+}
+
+int mmain ();
+{
+  cout << Map[5] [5] << endl;
+
+}
